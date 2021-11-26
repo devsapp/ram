18957694d0bb7ee480c368d0c13b2d45ec5c13d2
xsahxl <xsahxl@126.com>
2021-11-26 14:51:27 +0800

    feat: 🎸 update logger

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILogger } from '@serverless-devs/core';
-import { IInputs } from './interface';
-import Base from './common/base';
-export default class RamCompoent extends Base {
-    logger: ILogger;
-    deploy(inputs: IInputs): Promise<string>;
-    delete(inputs: any): Promise<void>;
-    remove(inputs: any): Promise<void>;
-}
